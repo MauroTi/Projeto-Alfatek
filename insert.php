712ced90d1cb2ca6cddccda36300b2c2cf84720b
Mauro Guterres <mauro4879@gmail.com>
2023-01-11 17:57:39 -0300

    Alterado banco nao funciona

--- insert.php
+++ insert.php
@@ -6,11 +6,12 @@ $id = mysqli_real_escape_string(conecta(), $_POST['id']);
 $nome = mysqli_real_escape_string(conecta(), $_POST['nome']);
 $codigo = mysqli_real_escape_string(conecta(), $_POST['codigo']);
 $quantidade = mysqli_real_escape_string(conecta(), $_POST['quantidade']);
-$os = mysqli_real_escape_string(conecta(), $_POST['os']);
+$nome = mysqli_real_escape_string(conecta(), $_POST['nome_pendencias']);
+$codigo = mysqli_real_escape_string(conecta(), $_POST['codigo_pendencias']);
+$quantidade = mysqli_real_escape_string(conecta(), $_POST['quantidade_pendencias']);
+$os = mysqli_real_escape_string(conecta(), $_POST['os_pendencias']);
 
-$query = "INSERT INTO pecas (id,nome,codigo,quantidade,os) VALUES ('$id','$nome','$codigo','$quantidade','$os')";
-
-$query = "INSERT INTO pecas (id,nome_pendencias,codigo_pendencias,quantidade_pendencias,os_pendencias) VALUES ('$id','$nome','$codigo','$quantidade','$os')";
+$query = "INSERT INTO pecas (id,nome,codigo,quantidade,nome_pendencias,codigo_pendencias,quantidade_pendencias,os_pendencias) VALUES ('$id','$nome','$codigo','$quantidade','$nome_pendencias','$codigo_pendencias','$quantidade_pendencias','$os_pendencias')";
 
 $resultado = mysqli_query(conecta(), $query);
 
