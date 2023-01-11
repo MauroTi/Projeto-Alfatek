f370f77caccd73766bf07c85c748d3e5f53436ca
Mauro Guterres <mauro4879@gmail.com>
2023-01-11 10:23:00 -0300

    correçoes aparencia 2

--- lista_pendencias.php
+++ lista_pendencias.php
@@ -53,13 +53,9 @@
 	
 	<div class="centro tabela-container" id="botoes">
 
-		<tr>
-			<td><button class="botoes_footer" style="border-radius: 10px ;"><a href="index.php" style="color: #000;">Início</a></button></td>
-			<td><button class="botoes_footer" style="border-radius: 10px;"><a href="cadastrar_estoque.php" style="color: #000;">Cadastrar Estoque</a></button></td>
-			<td><button class="botoes_footer" style="border-radius: 10px;"><a href="cadastrar_pendencias.php" style="color: #000;">Cadastrar Pendências</a></button></td>
-			<td><button class="botoes_footer" style="border-radius: 10px;"><a href="lista_pendencias.php" style="color: #000;">Lista Pendências</a></button></td>
-			<td><button class="botoes_footer" style="border-radius: 10px;"><a href="lista_estoque.php" style="color: #000;">Lista Estoque</a></button></td>
-		</tr>
+		 <?php
+                    include 'botoes.php';
+                    ?>
 
 	</div>
 	<table class="tabela tabela-container" border="1">
