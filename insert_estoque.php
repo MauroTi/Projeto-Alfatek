5b2638b9db20703d1c41a8709f91249c2dd523d7
Mauro <54038705+MauroTi@users.noreply.github.com>
2023-02-13 13:27:19 -0300

    verificação de campos vazios em pendencias 2

--- insert_estoque.php
+++ insert_estoque.php
@@ -2,17 +2,35 @@
 include('funcoes/conecta.php');
 
 $id = mysqli_real_escape_string(conecta(), $_POST['id']);
-$nome = mysqli_real_escape_string(conecta(), $_POST['nome']);
-$codigo = mysqli_real_escape_string(conecta(), $_POST['codigo']);
-$quantidade = mysqli_real_escape_string(conecta(), $_POST['quantidade']);
-$data_pecas = mysqli_real_escape_string(conecta(), $_POST['data_pecas']);
+if (!empty($_POST['nome'])) {
+    $nome = mysqli_real_escape_string(conecta(), $_POST['nome']);
+} else {
+    echo "<script>alert('Todos os campos devem ser preenchidos'); window.location.href = 'cadastrar.php';</script>";
+    exit;
+}
+
+if (!empty($_POST['codigo'])) {
+    $codigo = mysqli_real_escape_string(conecta(), $_POST['codigo']);
+} else {
+    echo "<script>alert('Todos os campos devem ser preenchidos'); window.location.href = 'cadastrar.php';</script>";
+    exit;
+}
+
+if (!empty($_POST['quantidade'])) {
+    $quantidade = mysqli_real_escape_string(conecta(), $_POST['quantidade']);
+} else {
+    echo "<script>alert('Todos os campos devem ser preenchidos'); window.location.href = 'cadastrar_estoque.php';</script>";
+    exit;
+}
+
+if (isset($_POST['data_pecas'])) {
+    $data_pecas = mysqli_real_escape_string(conecta(), $_POST['data_pecas']);
+}
+
 
 $query = "INSERT INTO pecas (id,nome,codigo,quantidade,data_pecas) VALUES ('$id','$nome','$codigo','$quantidade',now())";
 
 $resultado = mysqli_query(conecta(), $query);
-
-
-
 if ($resultado == true && $resultado != "") {
     echo "<script>alert('Cadastro efetuado com sucesso!'); window.location.href='lista_estoque.php';</script>";
 } else {
@@ -20,17 +38,10 @@ if ($resultado == true && $resultado != "") {
 }
 ?>
 <script>
- /* if ($resultado) {
-    // Redirect to the "pesquisa.php" page with a success message
-    window.location.replace('pesquisa.php?msg=Cadastro efetuado!');
-
-    // Display a confirmation message
-    window.alert('Cadastro efetuado com sucesso!');
-  } else {
-    // Display an error message
-    window.alert('Erro ao efetuar o cadastro. Por favor, tente novamente.');
-  }*/
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("nome").value = "<?php echo isset($_POST['nome']) ? $_POST['nome'] : ''; ?>";
+  document.getElementById("codigo").value = "<?php echo isset($_POST['codigo']) ? $_POST['codigo'] : ''; ?>";
+  document.getElementById("quantidade").value = "<?php echo isset($_POST['quantidade']) ? $_POST['quantidade'] : ''; ?>";
+  document.getElementById("data_pecas").value = "<?php echo isset($_POST['data_pecas']) ? $_POST['data_pecas'] : ''; ?>";
+});
 </script>
-
-
-
